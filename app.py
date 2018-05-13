a50732c396a5030b1762e512cb519d6eb0740bbd
Shravan <murachpersadshravan@icloud.com>
2018-05-13 14:31:14 +0400

    make it better

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from crontab import CronTab
 
 cron = CronTab(user='murachpersad')  
-job = cron.new(command='python /app/WeatherStation.py')  
-job.minute.every(15)
+# job = cron.new(command='python /app/WeatherStation.py')  
+# job.minute.every(15)
+
+Synoptic_Reader = cron.new(command='python /app/Test.py')  
+Synoptic_Reader.minute.every(1)
 
 #cron.remove_all()
 
